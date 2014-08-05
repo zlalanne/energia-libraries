8506221b9545fdd442687f9e4ae3dc9854a0d9d4
Zack Lalanne <zack.lalanne@gmail.com>
2014-08-04 20:55:52 -0500

    Adding some practice data for prototyping

--- app/js/controllers.js
+++ app/js/controllers.js
@@ -1,7 +1,24 @@
 (function() {
 	var libraryApp = angular.module('libraryApp', []);
-	libraryApp.controller('CategoryController', function() {
 
+	libraryApp.controller('CategoryController', function($scope, $http) {
+		$http.get("/app/data/categories.json").
+		  success(function(data) {
+			  $scope.categories = data.categories;
+		  });
 	});
 
+	libraryApp.controller('BoardController', function($scope, $http) {
+		$http.get("/app/data/boards.json").
+		  success(function(data) {
+		  	$scope.boards = data.boards;
+		  });
+	});
+
+	libraryApp.controller('LibraryController', function($scope, $http) {
+		$http.get("/app/data/libraries.json").
+		  success(function(data) {
+		  	$scope.libraries = data.libraries;
+		  });
+	});
 })();
