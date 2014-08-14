696576ececf74f37bd83a3441e3364204f0925d2
Zack Lalanne <zack.lalanne@gmail.com>
2014-08-14 00:14:53 -0500

    Addded simple routing for angular app

--- app/js/app.js
+++ app/js/app.js
@@ -1,3 +1,17 @@
 (function() {
-	var libraryApp = angular.module('libraryApp', ['angular-lodash']);
+  var libraryApp = angular.module('libraryApp', ['ngRoute', 'angular-lodash']);
+
+  libraryApp.config(['$routeProvider',
+    function($routeProvider) {
+      $routeProvider.
+        when('/', {
+          templateUrl: 'app/html/library-list.html'
+        }).
+        when('/library/:libraryId', {
+          templateUrl: 'app/html/library-detail.html'
+        }).
+        otherwise({
+          templateUrl: 'app/html/library-list.html'
+        });
+    }]);
 })();
--- app/js/controllers.js
+++ app/js/controllers.js
@@ -1,64 +1,64 @@
-(function() {
-	var libraryApp = angular.module('libraryApp');
+(function () {
+    var libraryApp = angular.module('libraryApp');
 
-	libraryApp.controller('CategoryController', function($scope, $http) {
-		$http.get("/app/data/categories.json").
-		  success(function(data) {
-			  $scope.categories = data.categories;
-		  });
-	});
+    libraryApp.controller('CategoryController', ['$scope', '$http', function($scope, $http) {
+        $http.get("/app/data/categories.json").
+          success(function(data) {
+              $scope.categories = data.categories;
+          });
+    }]);
 
-	libraryApp.controller('BoardController', function($scope, $http) {
-		$http.get("/app/data/boards.json").
-		  success(function(data) {
-		  	$scope.boards = data.boards;
-		  });
-	});
+    libraryApp.controller('BoardController', ['$scope', '$http', function($scope, $http) {
+        $http.get("/app/data/boards.json").
+          success(function(data) {
+            $scope.boards = data.boards;
+          });
+    }]);
 
-	libraryApp.controller('LibraryController', function($scope, $http) {
-		$http.get("/app/data/libraries.json").
-		  success(function(data) {
-		  	$scope.libraries = data.libraries;
-		  });
-	});
+    libraryApp.controller('LibraryController', ['$scope', '$http', function($scope, $http) {
+        $http.get("/app/data/libraries.json").
+          success(function(data) {
+            $scope.libraries = data.libraries;
+          });
+    }]);
 
-	libraryApp.controller('FilterController', function($scope) {
-		
-		$scope.categoryFilters = [];
-		$scope.boardFilters = [];
+    libraryApp.controller('FilterController', ['$scope', function($scope) {
+        
+        $scope.categoryFilters = [];
+        $scope.boardFilters = [];
 
-		this.toggleCategory = function(data) {
-			$scope.categoryFilters = _.xor($scope.categoryFilters, [data]);
-		};
+        this.toggleCategory = function(data) {
+            $scope.categoryFilters = _.xor($scope.categoryFilters, [data]);
+        };
 
-		this.toggleBoard = function(data) {
-			$scope.boardFilters = _.xor($scope.boardFilters, [data]);
-		};
+        this.toggleBoard = function(data) {
+            $scope.boardFilters = _.xor($scope.boardFilters, [data]);
+        };
 
-		this.isCategoryActive = function(data) {
-			return _.indexOf($scope.categoryFilters, data) !== -1;
-		};
+        this.isCategoryActive = function(data) {
+            return _.indexOf($scope.categoryFilters, data) !== -1;
+        };
 
-		this.isBoardActive = function(data) {
-			return _.indexOf($scope.boardFilters, data) !== -1;
-		};
+        this.isBoardActive = function(data) {
+            return _.indexOf($scope.boardFilters, data) !== -1;
+        };
 
-		this.filterResults = function(library) {
+        this.filterResults = function(library) {
 
-			// Add library to result list if no filters selected
-			if($scope.categoryFilters.length === 0 && $scope.boardFilters.length === 0) {
-				return true;
-			}
+            // Add library to result list if no filters selected
+            if($scope.categoryFilters.length === 0 && $scope.boardFilters.length === 0) {
+                return true;
+            }
 
-			if(_.intersection(library.categories, $scope.categoryFilters).length > 0) {
-				return true;
-			} else {
-				if(_.intersection(library.workingBoards, $scope.boardFilters).length > 0) {
-					return true;
-				} else {
-					return false;
-				}
-			}
-		};
-	});
+            if(_.intersection(library.categories, $scope.categoryFilters).length > 0) {
+                return true;
+            } else {
+                if(_.intersection(library.workingBoards, $scope.boardFilters).length > 0) {
+                    return true;
+                } else {
+                    return false;
+                }
+            }
+        };
+    }]);
 })();
